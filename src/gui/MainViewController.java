7758cbd4261af85d0b53c67c5cc8f33e1646416c
Gersen Jorge <gersen.jorge@gmail.com>
2019-01-21 11:23:03 -0200

    Main view controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -18,25 +18,20 @@ public class MainViewController implements Initializable {
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
-		
 	}
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
 		System.out.println("onMenuItemDepartmentAction");
-		
 	}
 
 	@FXML
 	public void onMenuItemAboutAction() {
 		System.out.println("onMenuItemAboutAction");
-		
 	}
 	
 
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
-		
 	}
-
 }
